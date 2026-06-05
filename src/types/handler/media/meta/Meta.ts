64999c22978fe0c7ad2c657732ec8203a8a2f7bf
d-najd <dimitar.hristijan.najdovski@gmail.com>
2026-06-05 13:23:58 +0200

    feat: adding more meta handler sdk data

--- src/types/handler/media/meta/Meta.ts
+++ src/types/handler/media/meta/Meta.ts
@@ -5,6 +5,12 @@ type Meta = {
 	readonly type: ResourceType | string
 	readonly name: string
 	readonly poster: string
+
+   readonly description?: string
+   readonly released?: string
+   readonly year?: string
+   readonly background?: string
+   readonly logo?: string
 }
 
 export default Meta
